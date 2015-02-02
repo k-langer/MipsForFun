cb019006c26e050639d8a9f6c5aeb673a356d097
Kevin <klanger@ccs.neu.edu>
2015-02-02 16:51:27 -0500

    Start tracking this project

---
+++ asm/asm.h
@@ -0,0 +1,328 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#define MAXLINELENGTH 1000
+#define ERROR 1
+#define WARNING 0 
+
+const unsigned int R_type    = 0b000000;
+const unsigned int LW_type   = 0b100011;
+const unsigned int SW_type   = 0b101011;
+const unsigned int BEQ_type  = 0b000100;
+const unsigned int ADDI_type = 0b001000;
+const unsigned int ANDI_type = 0b001100;
+const unsigned int ORI_type  = 0b001101;
+const unsigned int XORI_type = 0b001110;
+const unsigned int J_type    = 0b000010;
+const unsigned int BNE_type  = 0b000101; 
+ 
+const unsigned int SRLV_funct = 0b000110;
+const unsigned int SLLV_funct = 0b000100;
+const unsigned int ADD_funct  = 0b100000;
+const unsigned int SUB_funct  = 0b100010;
+const unsigned int AND_funct  = 0b100100;
+const unsigned int OR_funct   = 0b100101;
+const unsigned int SLT_funct  = 0b101010;
+const unsigned int XOR_funct  = 0b100110;
+char *label, *opcode, *arg0, *arg1, *arg2;
+char lineString[MAXLINELENGTH+1];
+typedef int flags_e;
+
+typedef struct BranchLabel {
+    char ** labels; 
+    int * addrs;
+    int length;
+    int pc;  
+} BranchLabel_t; 
+
+char * readAndParse(FILE *, char *, char **, char **, char **,
+    char **, char **);
+int isNumber(char *);
+int isRegister(char *);
+int isAddress(char *, int * addr);
+int getOpcode(char *);
+int getFunct(char *);
+int onesMask(int start, int length); 
+int fieldMask(int field, int start, int length); 
+int labelSizes(int * opcodeLength, int * labelLength, FILE * inFilePtr );
+int specialInstResolve(char **opcodePtr, char **arg0Ptr, char **arg1Ptr, char **arg2Ptr); 
+int getMachineCode(BranchLabel_t *pcs, int inst, char* opcode, char* arg0, char* arg1, char* arg2);
+int assignLabels(FILE *, BranchLabel_t *); 
+int branchResolveToInt(char * branch, BranchLabel_t * lbls);
+int updatePC(BranchLabel_t * branches); 
+void flag(char * msg, flags_e type);
+int jumpTo(char * jump, BranchLabel_t * lbls);
+
+void flag(char * msg, flags_e type) {
+
+    if (type == ERROR) {
+        printf("ERROR: %s!!\n",msg);
+        exit(1);
+    } 
+    printf("WARNING: %s!\n",msg);
+}
+int updatePC(BranchLabel_t * branches) {
+    branches->pc = branches->pc+4; 
+    return branches->pc;
+}
+int branchResolveToInt(char * branch, BranchLabel_t * lbls) {
+    for (int i = 0; i < lbls->length; i++) { 
+        if (!strcmp(branch, lbls->labels[i])) {
+            return (lbls->addrs[i]/4) - (lbls->pc/4);
+        }
+    }
+    
+    flag(branch,ERROR);
+    return 0; 
+}
+int assignLabels(FILE * inFilePtr, BranchLabel_t * branches) {
+    char ** labels = branches->labels; 
+    int * labelAddr = branches->addrs; 
+    int length = branches->length; 
+    char * neof = (char*) 1; 
+    int addr = 0;
+    length--;   
+    while (neof) {
+        neof = readAndParse(inFilePtr, lineString, 
+            &label, &opcode, &arg0, &arg1, &arg2);
+        if (neof) {
+            if (opcode) { 
+                if (label) { 
+                    if (length < 0) { return -1; } 
+                    labels[length] = malloc(strlen(label)*sizeof(char*));
+                    char * lb = labels[length];
+                    char * lb1 = label;
+                    memcpy(lb,lb1,strlen(label));
+                    labelAddr[length--] = addr; 
+                }     
+                addr += 4;  
+            }
+        }
+    }
+    rewind(inFilePtr); 
+}
+int specialInstResolve(char **opcodePtr, char **arg0Ptr, char **arg1Ptr, char **arg2Ptr) { 
+    if (!strcmp(*opcodePtr, "print")) {
+    }
+    if (!strcmp(*opcodePtr, "nop")) {
+    }
+    return -1;
+}
+
+int fieldMask(int field, int start, int length) { 
+    int mask = onesMask(start,length); 
+    mask &= field << start;
+    return mask;
+} 
+int labelSizes(int * opcodeLength, int * labelLength, FILE * inFilePtr ) {
+    int len;
+    *labelLength =0; 
+    *opcodeLength = 0;
+    char * neof = (char *) 1;
+    while (neof) {
+        neof = readAndParse(inFilePtr, lineString, 
+            &label, &opcode, &arg0, &arg1, &arg2);
+            if (neof && opcode) {
+                if (label) {
+                    (*labelLength)++;
+                }
+                *opcodeLength++;
+            }
+    }
+    rewind(inFilePtr);
+}
+
+int onesMask(int start, int length) { 
+    int i, ret = 0; 
+    for (i = 0; i < length; i++) {
+        ret |= 1 << (start+i);
+    }
+    return ret; 
+} 
+
+char * readAndParse(FILE *inFilePtr, char *lineString,
+    char **labelPtr, char **opcodePtr, char **arg0Ptr,
+    char **arg1Ptr, char **arg2Ptr)
+{
+    char *statusString, *firsttoken;
+    statusString = fgets(lineString, MAXLINELENGTH, inFilePtr);
+    if (statusString != NULL) {
+        firsttoken = (char *) strtok(lineString, " \t\n");
+        if (firsttoken == NULL || firsttoken[0] == '#') {
+            return readAndParse(inFilePtr, lineString, labelPtr, opcodePtr, arg0Ptr, arg1Ptr, arg2Ptr);
+        } else if (firsttoken[strlen(firsttoken) - 1] == ':') {
+            *labelPtr = firsttoken;
+            *opcodePtr = (char *) strtok(NULL, " \t\n");
+            firsttoken[strlen(firsttoken) - 1] = '\0';
+        } else {
+            *labelPtr = NULL;
+            *opcodePtr = firsttoken;
+        }
+        *arg0Ptr = (char *) strtok(NULL, ", \t\n");
+        *arg1Ptr = (char *) strtok(NULL, ", \t\n");
+        *arg2Ptr = (char *) strtok(NULL, ", \t\n");
+    }
+    return(statusString);
+}
+
+int isRegister(char *string) {
+    if (!string) { return -1; }
+    int regnum;
+    int ret = ( (sscanf(string, "$%d", &regnum)) == 1) &&
+        regnum >= 0 &&
+        regnum < 16;
+    if (ret == 1) { return regnum; } 
+    flag("Not a valid register",ERROR); 
+    return -1; 
+}
+
+int isAddress(char *string, int * addr) {
+    if (!string) { return -1; }
+    int regnum;
+    int ret = ( (sscanf(string, "%d($%d)", addr, &regnum)) == 2) &&
+        regnum >= 0 &&
+        regnum < 16 &&
+        *addr   >= 0 &&
+        *addr  < 65536;
+    if (ret == 1) { return regnum; }
+    flag("Not a valid address",ERROR); 
+     return -1; 
+
+}
+    
+int isNumber(char *string) {
+    if (!string) { return -1; }
+    int i;
+    if ( (sscanf(string, "%d", &i)) == 1) {
+        return i;
+    }
+    flag("Not a number",ERROR); 
+    return -1;
+}
+int getFunct(char * opcode) {
+    if (getOpcode(opcode)!=R_type) { return -1; }
+    if (!strcmp(opcode, "add")) {
+        return ADD_funct; 
+    }
+    if (!strcmp(opcode, "srlv")) {
+        return SRLV_funct; 
+    }
+    if (!strcmp(opcode, "sllv")) {
+        return SLLV_funct; 
+    }
+    if (!strcmp(opcode, "or")) {
+        return OR_funct;
+    }
+    if (!strcmp(opcode, "and")) {
+        return AND_funct; 
+    }
+    if (!strcmp(opcode, "slt")) {
+        return SLT_funct; 
+    }
+    if (!strcmp(opcode, "sub")) {
+        return SUB_funct; 
+    }
+    if (!strcmp(opcode, "xor")) {
+        return XOR_funct; 
+    }
+    flag("Instruction not supported",WARNING); 
+    return -1;
+}
+int getOpcode(char * opcode) { 
+    
+    if (!opcode) { return -1; }
+    if (!strcmp(opcode, "addi")) {
+        return ADDI_type; 
+    }
+    if (!strcmp(opcode, "ori")) {
+        return ORI_type; 
+    }
+    if (!strcmp(opcode, "xori")) {
+        return XORI_type; 
+    }
+    if (!strcmp(opcode, "andi")) {
+        return ANDI_type; 
+    }
+    if (!strcmp(opcode, "add")) {
+        return R_type; 
+    }
+    if (!strcmp(opcode, "or")) {
+        return R_type;
+    }
+    if (!strcmp(opcode, "xor")) {
+        return R_type;
+    }
+    if (!strcmp(opcode, "and")) {
+        return R_type; 
+    }
+    if (!strcmp(opcode, "slt")) {
+        return R_type; 
+    }
+    if (!strcmp(opcode, "sub")) {
+        return R_type; 
+    }
+    if (!strcmp(opcode, "srlv")) {
+        return R_type; 
+    }
+    if (!strcmp(opcode, "sllv")) {
+        return R_type; 
+    }
+    if (!strcmp(opcode, "sw")) {
+        return SW_type;
+    }
+    if (!strcmp(opcode, "lw")) {
+        return LW_type;
+    }
+    if (!strcmp(opcode, "j")) {
+        return J_type;
+    }
+    if (!strcmp(opcode, "beq")) {
+        return BEQ_type;
+    }
+    if (!strcmp(opcode, "bne")) {
+        return BNE_type;
+    }
+    flag("Instruction not supported",WARNING); 
+    return -1;
+}
+int getMachineCode(BranchLabel_t *pcs, int inst, char* opcode, char* arg0, char* arg1, char* arg2) { 
+       int word = fieldMask(inst, 26, 6); 
+       if (inst == R_type) {
+            word |= fieldMask(isRegister(arg1),21,5); 
+            word |= fieldMask(isRegister(arg2),16,5); 
+            word |= fieldMask(isRegister(arg0),11,5); 
+            word |= fieldMask(getFunct(opcode),0,11);
+        }
+        if (inst == LW_type || inst == SW_type) {
+            int addr; 
+            int s = isAddress(arg1, &addr);
+            word |= fieldMask(s,21,5); 
+            word |= fieldMask(isRegister(arg0),16,5); 
+            word |= fieldMask(addr,0,16); 
+        }
+        if (inst == ADDI_type || inst == ANDI_type 
+            || inst == ORI_type || inst == XORI_type) {
+            word |= fieldMask(isRegister(arg1),21,5); 
+            word |= fieldMask(isRegister(arg0),16,5); 
+            word |= fieldMask(isNumber(arg2),0,16); 
+        }
+        if (inst == BEQ_type || inst == BNE_type) {
+            word |= fieldMask(isRegister(arg0),21,5);
+            word |= fieldMask(isRegister(arg1),16,5); 
+            word |= fieldMask(branchResolveToInt(arg2,pcs),0,16); 
+        }
+        if (inst == J_type) {
+            word |= fieldMask(jumpTo(arg0,pcs),0,26);
+        } 
+        return word; 
+}
+int jumpTo(char * jump, BranchLabel_t * lbls) {
+    for (int i = 0; i < lbls->length; i++) {
+        int addr = 0;  
+        if (!strcmp(jump, lbls->labels[i])) {
+            return lbls->addrs[i]/4;
+        }
+    }    
+}
+
+
---
+++ build/logs/mips.v
@@ -0,0 +1,271 @@
+module top(input        clk, reset, 
+           output [31:0] writedata, dataadr, 
+           output        memwrite);
+
+  wire [31:0] pc, instr, readdata;
+  // instantiate processor and memories
+  mips mips(clk, reset, pc, instr, memwrite, dataadr, 
+            writedata, readdata);
+  imem imem(pc[7:2], instr);
+  dmem dmem(clk, memwrite, dataadr, writedata, readdata);
+endmodule
+
+module dmem(input        clk, we,
+            input [31:0] a, wd,
+            output [31:0] rd);
+
+  reg [31:0] RAM[63:0];
+
+  assign rd = RAM[a[31:2]]; // word aligned
+
+  always @(posedge clk)
+    if (we) RAM[a[31:2]] <= wd;
+endmodule
+
+module imem(input [5:0] a,
+            output [31:0] rd);
+
+  reg [31:0] RAM[63:0];
+
+
+  assign rd = RAM[a]; // word aligned
+endmodule
+
+module mips(input        clk, reset,
+            output [31:0] pc,
+            input [31:0] instr,
+            output        memwrite,
+            output [31:0] aluout, writedata,
+            input [31:0] readdata);
+
+  wire       memtoreg, alusrc, regdst, 
+              regwrite, jump, pcsrc, zero;
+  wire [2:0] alucontrol;
+  controller c(instr[31:26], instr[5:0], zero,
+               memtoreg, memwrite, pcsrc,
+               alusrc, regdst, regwrite, jump,
+               alucontrol);
+  datapath dp(clk, reset, memtoreg, pcsrc,
+              alusrc, regdst, regwrite, jump,
+              alucontrol,
+              zero, pc, instr,
+              aluout, writedata, readdata);
+endmodule
+
+module controller(input [5:0] op, funct,
+                  input       zero,
+                  output       memtoreg, memwrite,
+                  output       pcsrc, alusrc,
+                  output       regdst, regwrite,
+                  output       jump,
+                  output [2:0] alucontrol);
+
+  wire [2:0] aluop;
+  wire       branch;
+  wire       branch_ne; 
+    
+  maindec md(op, memtoreg, memwrite, branch, branch_ne,
+             alusrc, regdst, regwrite, jump, aluop);
+  aludec  ad(funct, aluop, alucontrol);
+  assign pcsrc = ( branch & zero ) | (branch_ne & !zero);
+endmodule
+
+module maindec(input [5:0] op,
+            output       memtoreg, memwrite,
+            output       branch, branch_ne, alusrc,
+            output       regdst, regwrite,
+            output       jump,
+            output [2:0] aluop);
+
+reg [10:0] controls;
+
+    assign {regwrite, regdst, alusrc, branch, branch_ne,
+         memwrite, memtoreg, jump, aluop} = controls;
+    always @ (op)
+    case(op)
+        6'b000000: controls <= 11'b11000000100; // RTYPE
+        6'b100011: controls <= 11'b10100010000; // LW
+        6'b101011: controls <= 11'b00100100000; // SW
+        6'b000100: controls <= 11'b00010000010; // BEQ
+        6'b000101: controls <= 11'b00001000010; // BNE
+        6'b001000: controls <= 11'b10100000000; // ADDI
+        6'b001100: controls <= 11'b10100000001; // ANDI
+        6'b001101: controls <= 11'b10100000011; // ORI
+        6'b001110: controls <= 11'b10100000101; // XORI
+        6'b000010: controls <= 11'b00000001000; // J
+        default:   controls <= 11'bxxxxxxxxxxx; // illegal op
+    endcase
+endmodule
+
+module aludec(input [5:0] funct,
+              input [2:0] aluop,
+              output [2:0] alucontrol);
+    reg [2:0] alucontrol;
+
+    always @ (aluop or funct)
+    case(aluop)
+        3'b000: alucontrol <= 3'b010;  // add (for lw/sw/addi)
+        3'b010: alucontrol <= 3'b110;  // sub (for beq)
+        3'b001: alucontrol <= 3'b000;  // and (for andi)
+        3'b011: alucontrol <= 3'b001;  // or (for ori)
+        3'b101: alucontrol <= 3'b001;  // or (for ori)
+        default: case(funct)          // R-type instructions
+            6'b100100: alucontrol <= 3'b000; // and
+            6'b100101: alucontrol <= 3'b001; // or
+            6'b100000: alucontrol <= 3'b010; // add
+            6'b000100: alucontrol <= 3'b011; // <<
+            6'b000110: alucontrol <= 3'b100; // >>
+            6'b100110: alucontrol <= 3'b101; // xor
+            6'b100010: alucontrol <= 3'b110; // sub
+            6'b101010: alucontrol <= 3'b111; // slt
+            default:   alucontrol <= 3'bxxx; // ???
+    endcase
+    endcase
+endmodule
+
+module datapath(input        clk, reset,
+                input        memtoreg, pcsrc,
+                input        alusrc, regdst,
+                input        regwrite, jump,
+                input [2:0]  alucontrol,
+                output        zero,
+                output [31:0] pc,
+                input [31:0] instr,
+                output [31:0] aluout, writedata,
+                input [31:0] readdata);
+
+  wire [4:0]  writereg, writeaddr;
+  wire [31:0] pcnext, pcplus4, pcbranch;
+  wire [31:0] pcnextj; 
+  wire [31:0] signimm, signimmsh;
+  wire [31:0] srca, srcb;
+  wire [31:0] result;
+  wire link; 
+
+  // Jump and link control
+  assign link = jump & pcsrc;
+  // next PC wire
+  dff #(32) pcreg(clk, reset, pcnext, pc);
+  adder       pcadd1(pc, 32'b100, pcplus4);
+  sl2         immsh(signimm, signimmsh);
+  adder       pcadd2(pcplus4, signimmsh, pcbranch);
+  
+  mux2 #(32) pcjmpmux(pcplus4, {pcplus4[31:28],instr[25:0],2'b00},
+                                jump, pcnextj);
+  mux2 #(32) pcmux(pcnextj, pcbranch, pcsrc, pcnext); 
+  // register file wire
+  regfile     rf(clk, regwrite, instr[25:21], instr[20:16], 
+                 writeaddr, result, srca, writedata);
+  //mux2 (input [WIDTH-1:0] d0, d1,input s, output [WIDTH-1:0] y);
+  mux2 #(5)   wrmux(instr[20:16], instr[15:11],
+                    regdst, writeaddr);
+  //mux2 #(5)   linkmux(writereg,5'd31,link,writeaddr);
+
+  mux2 #(32)  resmux(aluout, readdata, memtoreg, result);
+  signext     se(instr[15:0], signimm);
+
+  // ALU wire
+  mux2 #(32)  srcbmux(writedata, signimm, alusrc, srcb);
+  alu         alu(srca, srcb, alucontrol, aluout, zero);
+endmodule
+
+module regfile(input        clk, 
+               input        we3, 
+               input [4:0]  ra1, ra2, wa3, 
+               input [31:0] wd3, 
+               output [31:0] rd1, rd2);
+  reg [31:0] rf[31:0];
+  
+
+  // three ported register file
+  // read two ports combinationally
+  // write third port on rising edge of clk
+  // register 0 hardwired to 0
+  // note: for pipelined processor, write third port
+  // on falling edge of clk
+
+  always @(posedge clk)
+    if (we3) begin
+        rf[wa3] <= wd3;	
+    end
+
+  assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
+  assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
+endmodule
+
+module adder(input [31:0] a, b,
+             output [31:0] y);
+
+  assign y = a + b;
+endmodule
+
+module sl2(input [31:0] a,
+           output [31:0] y);
+
+  // shift left by 2
+  assign y = {a[29:0], 2'b00};
+endmodule
+
+module signext(input [15:0] a,
+               output [31:0] y);
+              
+  assign y = {{16{a[15]}}, a};
+endmodule
+
+module dff #(parameter WIDTH = 8)
+              (input             clk, reset,
+               input [WIDTH-1:0] d, 
+               output [WIDTH-1:0] q);
+  reg [WIDTH-1:0] q; 
+  always @(posedge clk, posedge reset)
+    if (reset) q <= 0;
+    else       q <= d;
+endmodule
+
+module mux2 #(parameter WIDTH = 8)
+             (input [WIDTH-1:0] d0, d1, 
+              input             s, 
+              output [WIDTH-1:0] y);
+
+  assign y = s ? d1 : d0; 
+endmodule
+
+module mux4 #(parameter WIDTH = 8)
+             (input [WIDTH-1:0] d0, d1, d2, d3, 
+              input [1:0]        s, 
+              output [WIDTH-1:0] y);
+
+  assign y = s[1] ? 
+             s[0] ? d3 : d2: 
+             s[0] ? d1 : d0; 
+endmodule
+
+module alu(input [31:0] a, b,
+           input [2:0]  alucontrol,
+           output [31:0] result,
+           output        zero);
+  wire [31:0] condinvb, sum;
+
+  assign condinvb = alucontrol[2] ? ~b : b;
+  assign sum = a + condinvb + alucontrol[2];
+  
+  reg [31:0] result; 
+
+    
+    always @ *
+        case (alucontrol[2:0])
+          3'b000: result = a & b;
+          3'b001: result = a | b;
+          3'b101: result = a ^ b; 
+          3'b011: result = a << b;
+          3'b101: result = b << a;  
+          3'b100: result = a >> b; 
+          3'b110: result = {32{a[31]}}; 
+          3'b010: result = sum;
+          3'b110: result = sum;
+          3'b111: result = sum[31];
+          default: result = 32'bx; 
+        endcase
+
+  assign zero = (result == 32'b0);
+endmodule
